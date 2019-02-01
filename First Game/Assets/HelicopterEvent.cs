ad8601c95af157fa2103516f1969190754a719b4
Bryce <brhowarth@outlook.com>
2019-02-01 03:28:20 -0700

    First Game Updates

---
+++ First Game/Assets/HelicopterEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HelicopterEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    private void OnCollisionEnter2D()
+    {
+        Event.Invoke();
+        
+    }
+}
