650a3c996cbecf688d330e41b928765d482ddc61
Bryce <brhowarth@outlook.com>
2019-02-04 11:24:58 -0700

    First Game Updates

---
+++ First Game/Assets/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+
+    public string Name;
+
+    public Color SkinColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
