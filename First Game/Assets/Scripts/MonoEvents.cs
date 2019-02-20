c344ec0ecb5a1816d05cf15936ffe258584b8279
Bryce <brhowarth@outlook.com>
2019-02-20 11:23:53 -0700

    First Game Updates

---
+++ First Game/Assets/Scripts/MonoEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
+
+public class MonoEvents : MonoBehaviour
+{
+    public UnityEvent StartEvent, EnableEvent, MouseDownEvent
+        , UpdateEvent;
+    
+    void Start()
+    {
+        StartEvent.Invoke();
+    }
+    
+    private void OnEnable()
+    {
+        
+    }
+    
+    private void OnMouseDown()
+    {
+        MouseDownEvent.Invoke();
+    }
+
+    void Update()
+    {
+        
+    }
+}
