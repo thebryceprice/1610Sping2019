ab4f6da1961a109f4407da5e5462a9b2838e9c88
Bryce <brhowarth@outlook.com>
2019-02-27 11:22:38 -0700

    First Game Updates

---
+++ First Game/Assets/Scripts/ConditionalsInClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Conditionals : MonoBehaviour
+{
+    public UnityEvent OnEvent, OffEvent;
+
+    public bool OnBool;
+
+    public int Number = 11;
+
+    public string Password;
+
+    private void Update()
+{
+        if(OnBool){}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ First Game/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public UnityEvent OnGrounded, OffGrounded; 
+    
+    public float Speed = 3;
+     
+    private CharacterController controller;
+    private Vector3 position;
+    
+   
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            OnGrounded.Invoke();
+        }
+        else
+        {
+            OffGrounded.Invoke();
+        }
+        
+        position.y = Input.GetAxis("Vertical")*Speed*Time.deltaTime;
+        position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        
+        controller.Move(position);
+    }
+}
