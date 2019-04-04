1ebf75e95517323e11c1f346ea136bc427f0cb10
Bryce <brhowarth@outlook.com>
2019-04-03 23:12:15 -0600

    First Game Updates

---
+++ First Game/Assets/Scripts/GameMenus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameMenus : MonoBehaviour
+{
+    public bool mouseClick;
+
+
+    //When ESC game is clicked...bring up Pause Menu
+    //Pause Menu Controls
+    public void OnMouseDown()
+    {
+        if (mouseClick != true)
+        {
+            //Resume Game when Resume button is clicked
+        }
+        
+        //Restart Game when Restart button is clicked
+        if (mouseClick != true)
+        {
+            Application.LoadLevel("SampleScene");          
+        }
+
+        
+        //End game (stop Unity) when Exit Game button is clicked
+        if (mouseClick != true)
+        {
+            //End Game
+        }
+    }
+}
