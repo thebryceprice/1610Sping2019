412d8eda35ef4d5c833c2910c142be085145f5e8
Bryce <brhowarth@outlook.com>
2019-04-07 20:09:37 -0600

    First Game Updates

---
+++ First Game/Assets/Scripts/Destructibles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructibles : MonoBehaviour
+{
+    //When Bullets collide with an object tagged "Destructible
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            //Play Destructible's animation when bullet collides
+            
+        }
+    }
+}
