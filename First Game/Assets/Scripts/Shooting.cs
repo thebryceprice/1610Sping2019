78aa550bac5f5960f135c2d826d93b96e52bbfd3
Bryce <brhowarth@outlook.com>
2019-04-16 23:03:24 -0600

    First Game Updates

--- First Game/Assets/Scripts/Shooting.cs
+++ First Game/Assets/Scripts/Shooting.cs
@@ -1,17 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
+    //tell where bullet is shooting from
+    public GameObject bulletEmitter;
+    //instance
+    public GameObject bullet;
+    //forward push on instance
+    public float bulletMomentum;
 
-    public int shoot;
-    public float speed;
-    public float bulletAngle;
-    
-//On Mouse Down, Shoot a Bullet from Gun in direction of cursor
-    public void OnMouseDown()
+    private void Start()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //bullet instantiation
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
+            
+            //correction of incorrectly rotated bullets
+            //Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+
+            //call the rigidbody of created bullets
+            Rigidbody Temporary_Rigidbody;
+            Temporary_Rigidbody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+            
+            
+            
+            
+            //bullets are being pushed by the amount set
+            Temporary_Rigidbody.AddForce(transform.right * bulletMomentum);
+            
+            
+            
+            
+            //destruction of bullets after 10 seconds
+            Destroy(Temporary_Bullet_Handler, 10.0f);
+
+
+
+
+
+        }
     }
 }
