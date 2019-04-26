37c47b3e96ba520acec7523be0f97ca714949c5e
Bryce <brhowarth@outlook.com>
2019-04-25 20:55:56 -0600

    First Game Updates
    Game restart on death system has broken - there are no errors, it just infinitely loads the scene. Think its the "DONTDESTROYONLOAD" code

--- First Game/Assets/Scripts/EnemyAI.cs
+++ First Game/Assets/Scripts/EnemyAI.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private NavMeshAgent _nav;
+   private Transform _player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    //When Enemy enters screen, begin shooting at player
+   
+   //AI MOVEMENT from Unity Tutorial
+   private void Start()
+   {
+      //Bar Enemy
+      _nav = GetComponent<NavMeshAgent>();
+      //Keep track of the players xyz coordinates
+      _player = GameObject.FindGameObjectWithTag("Player").transform;
+   }
+
+   private void Update()
+   {
+      //Enemy will chase player by trying to match Players position
+      _nav.SetDestination(_player.position);
+   }
 }
