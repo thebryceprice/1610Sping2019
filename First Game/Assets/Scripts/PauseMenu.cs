d2697f6b9e57536b1b5fb09b071eda677a25117a
Bryce <brhowarth@outlook.com>
2019-04-28 18:27:02 -0600

    First Game Updates

---
+++ First Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    //can be viewed by other scripts, BUT they can only check
+    public static bool gamePaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    void Resume ()
+    {
+        //pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+
+    void Pause ()
+    {
+        //PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    
+    
+    
+}
+
+
